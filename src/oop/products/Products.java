0e23d2e1dd419b9fec4d73ac40dcbcb5c033f955
ondrej.wislocki <ondrej.wislocki@infis.cz>
2026-09-24 13:20:11 +0200

    21.09

--- src/oop/products/Products.java
+++ src/oop/products/Products.java
@@ -9,13 +9,14 @@ public class Products {
         DataImport di = new DataImport("data/products.txt");
         ArrayList<Product> allProducts = new ArrayList<>();
         Long allProductsCount = 0L;
-        while (di.hasNext()){
+        while (di.hasNext()) {
             String[] line = di.readLine().split(";");
-            if (line.length == 4) allProducts.add(new Product(line[0], line[1], Integer.parseInt(line[2]), Double.parseDouble(line[3]) ));
+            if (line.length == 4)
+                allProducts.add(new Product(line[0], line[1], Integer.parseInt(line[2]), Double.parseDouble(line[3])));
             if (line.length == 3) allProducts.add(new Product(line[0], line[1], Integer.parseInt(line[2])));
-            if (line.length == 2){new Product(line[0], line[1]);}
+            if (line.length == 2) new Product(line[0], line[1]);
 
-            if (line.length>2) {
+            if (line.length > 2) {
                 System.out.println("Počet kusů: " + line[2]);
                 allProductsCount += Long.parseLong(line[2]);
             }
